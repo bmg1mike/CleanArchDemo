529d3b10a635d311e0c063ed9e5b65eefb7f7423
boniface michael <boniface.michael@thegiggroupng.com>
2020-10-23 10:48:02 +0100

    Application implementation

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Infra.Data.Context;
+using Core.Models;
+using Core.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _universityDbContext;
+        public CourseRepository(UniversityDbContext universityDbContext)
+        {
+            _universityDbContext = universityDbContext;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            var courses = _universityDbContext.Courses;
+            return courses;
+        }
+    }
+}
---
+++ CleanArch/Core/Models/Interfaces/ICourseRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
